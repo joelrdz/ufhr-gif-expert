3d9d6d8bb0d1d49e4b5696f9b1aa420535da61ad
Joel Rdz <joelrdzh@gmail.com>
2022-07-13 19:34:53 -0500

    Gif Expert App - Initial commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,11 @@
+export const GifExpertApp = () => {
+  return <>
+    {/* Title */}
+    <h1>GifExpertApp</h1>
+
+    {/* Input */}
+
+    {/* List of Gifs */}
+      {/* Gif item * n */}
+  </>;
+};
