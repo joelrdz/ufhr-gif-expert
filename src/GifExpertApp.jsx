d9e887d84000f45504f4d521754388c634beb2f4
Joel Rdz <joelrdzh@gmail.com>
2022-07-13 20:35:51 -0500

    handleAddCategory function

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -3,6 +3,11 @@ import { useState } from 'react';
 export const GifExpertApp = () => {
   const [categories, setCategories] = useState(['Dragon Ball', 'Demon Slayer']);
 
+  const handleAddCategory = () => {
+    setCategories(['Death Note', ...categories]);
+    // setCategories(cat => [...cat, 'Death Note']);
+  };
+
   return <>
     {/* Title */}
     <h1>GifExpertApp</h1>
@@ -10,6 +15,7 @@ export const GifExpertApp = () => {
     {/* Input */}
 
     {/* List of Gifs */}
+    <button onClick={ handleAddCategory }>Add</button>
     <ol>
       {categories.map(category => (
         <li key={ category }>{ category }</li>
