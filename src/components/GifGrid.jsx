e2db0bad1149091988982922323538c55fc94283
Joel Rdz <joelrdzh@gmail.com>
2022-07-20 19:36:23 -0500

    useEffect basic approach
    - run API Call just on component mount (good practice)

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,11 +1,18 @@
+import { useState, useEffect } from 'react';
 import { getGifs } from '../helpers/getGifs';
 
 export const GifGrid = ({ category }) => {
-  getGifs(category);
+  const [counter, setCounter] = useState(10);
+
+  useEffect(() => {
+    getGifs(category);
+  }, []);
 
   return (
     <>
       <h3>{ category }</h3>
+      <div>{ counter }</div>
+      <button onClick={ () => setCounter(counter + 1) }>+1</button>
     </>
   );
 };
--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,7 +1,7 @@
 const apiKey = 'a87UP8mV8cYgYGmqfpKyC8xT1KZXTFG3';
 
 export const getGifs = async ( category ) => {
-  const url = `https://api.giphy.com/v1/gifs/search?api_key=${ apiKey }&q=${ category }&limit=20`;
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=${ apiKey }&q=${ category }&limit=10`;
   const response = await fetch(url);
   const { data } = await response.json();
 
@@ -11,6 +11,5 @@ export const getGifs = async ( category ) => {
     url: gif.images.downsized_medium.url
   }));
 
-  console.log(gifs);
   return gifs;
 };
